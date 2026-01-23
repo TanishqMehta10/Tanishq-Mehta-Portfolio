2a4bca755d8ed956f2723bd983f62d7850abc5b4
Tanishq Mehta <tanishqamehta1@gmail.com>
2026-01-23 12:28:47 +0530

    first commit

---
+++ Portfolio/src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Navigation from './components/Navigation';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Certifications from './components/Certifications';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Background3D from './components/Background3D';
+import CursorAnimation from './components/CursorAnimation';
+
+function App() {
+  return (
+    <div className="relative min-h-screen bg-gray-900 overflow-x-hidden">
+      
+      <Background3D />
+      <CursorAnimation />
+      <Navigation />
+        <About />
+      <Skills />
+      <Projects />
+      <Certifications />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
